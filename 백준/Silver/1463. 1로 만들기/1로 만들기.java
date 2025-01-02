85a25d7e7b14b15feb4cdf7fce6c0761809f02e5
Su Young <tnehd83@naver.com>
2025-01-02 14:16:42 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 18132 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,29 +1,19 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.HashMap;
 
 public class Main {
-    private static int min = Integer.MAX_VALUE;
-
-    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static StringTokenizer st;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     static int[] d = new int[1000001];
+    static int n;
 
     public static void main(String[] args) throws IOException {
-        st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-
-        dfs(n);
-        System.out.println(d[n]);
-    }
-
-    public static void dfs(int sum) {
-        for (int i = 2; i <= sum; i++) {
-            d[i] = d[i - 1] + 1;
-            if (i % 2 == 0) d[i] = Math.min(d[i], d[i / 2] + 1);
-            if (i % 3 == 0) d[i] = Math.min(d[i], d[i / 3] + 1);
+        n = Integer.parseInt(br.readLine());
+        for(int i = 2; i <= n; i++) {
+            d[i] = d[i-1] + 1;
+            if(i % 2 == 0) d[i] = Math.min(d[i], d[i/2] + 1);
+            if(i % 3 == 0) d[i] = Math.min(d[i], d[i/3] + 1);
         }
+        System.out.println(d[n]);
     }
 }
