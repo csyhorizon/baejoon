a7592bef336d78752de4359781e8dcb38f8e81bc
Su Young <tnehd83@naver.com>
2025-01-03 13:01:12 +0900

    [Gold V] Title: 0 만들기, Time: 156 ms, Memory: 17120 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.java
+++ 백준/Gold/7490. 0 만들기/0 만들기.java
@@ -3,98 +3,32 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
-import java.util.Vector;
 
 public class Main {
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    public static StringTokenizer st;
-    public static int nowSize = 0;
+    public static int n;
 
     public static void main(String[] args) throws IOException {
-        int N = Integer.parseInt(br.readLine());
-//        1 .. N
-        for (int i = 0; i < N; i++) {
-            nowSize = Integer.parseInt(br.readLine());
-            Vector<String> vec = new Vector<>();
-            vec.add("1");
-            dfs(vec, 2);
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            n = Integer.parseInt(br.readLine());
+            sol(1, 1, 1, 0, "1");
             bw.write("\n");
         }
-
         bw.close();
     }
 
-    public static void dfs(Vector<String> vec, int position) throws IOException {
-        if (position == nowSize + 1) {
-            print(vec);
-        }
-        else {
-            vec.add(" ");
-            vec.add(position + "");
-            dfs(vec, position + 1);
-            vec.remove(vec.size() - 1);
-            vec.remove(vec.size() - 1);
-
-            vec.add("+");
-            vec.add(position + "");
-            dfs(vec, position + 1);
-            vec.remove(vec.size() - 1);
-            vec.remove(vec.size() - 1);
-
-            vec.add("-");
-            vec.add(position + "");
-            dfs(vec, position + 1);
-            vec.remove(vec.size() - 1);
-            vec.remove(vec.size() - 1);
-        }
-    }
-
-    public static void print(Vector<String> vec) throws IOException {
-        int result = 0;
-        String lastInput = "";
-        int select = 0;
-
-        for(String s : vec) {
-            if (s.equals("+")) {
-                if (select == 0) {
-                    result += Integer.parseInt(lastInput);
-                }
-                else {
-                    result -= Integer.parseInt(lastInput);
-                }
-                lastInput = "";
-                select = 0;
+    public static void sol(int d, int stack, int def_sign, int sum, String str) throws IOException {
+        if (d == n) {
+            sum += (stack * def_sign);
+            if (sum == 0) {
+                bw.write(str + "\n");
             }
-            else if (s.equals("-")) {
-                if (select == 0) {
-                    result += Integer.parseInt(lastInput);
-                }
-                else {
-                    result -= Integer.parseInt(lastInput);
-                }
-                lastInput = "";
-                select = 1;
-            }
-            else if (s.equals(" ")) {}
-            else {
-                lastInput += s;
-            }
+            return;
         }
-
-        if (select == 0) {
-            result += Integer.parseInt(lastInput);
-        }
-        else {
-            result -= Integer.parseInt(lastInput);
-        }
-
-        if (result == 0) {
-            for(String s : vec) {
-                bw.write(s);
-            }
-            bw.write("\n");
-        }
+        sol(d + 1, stack * 10 + d + 1, def_sign, sum, str + " " + (d + 1));
+        sol(d + 1, d + 1, 1, sum + ((stack) * def_sign), str + "+" + (d + 1));
+        sol(d + 1, d + 1, -1, sum + ((stack) * def_sign), str + "-" + (d + 1));
     }
 }
